5c8af3854c242ebc53f8ba8fd03865a6b568062d
trucanh <anhtructran111@gmail.com>
2021-09-11 11:53:48 +0700

    handle admin page for website

--- app/controllers/admin/manage_user_controller.rb
+++ app/controllers/admin/manage_user_controller.rb
@@ -1,5 +1,24 @@
 class Admin::ManageUserController < AdminApplicationController
-  def new_user
 
+  def show_users
+    @users = User.all
+  end
+
+  def new_user
+    @user = User.new
+  end
+
+  def create_user
+    user = User.new(user_params)
+    if user.save
+      redirect_to admin_users_path, notice: "Tạo user thành công"
+    else
+      render json: {error: "Không thể tạo user"}, status: :not_found
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :sex, :is_admin)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'dashboard', to: "dashboard#index"
     #user
+    get 'users', to: "manage_user#show_users"
     get 'new_user', to: "manage_user#new_user"
+    post 'create_user', to: "manage_user#create_user"
   end
   root to: 'home#index'
   devise_for :users
