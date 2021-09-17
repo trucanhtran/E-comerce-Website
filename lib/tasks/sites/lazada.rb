8c580f5fc4278feb9779f9b558602943b78ea032
trucanh <anhtructran111@gmail.com>
2021-09-17 10:10:04 +0700

    handle crawl data from lazada

--- lib/tasks/sites/lazada.rb
+++ lib/tasks/sites/lazada.rb
@@ -18,9 +18,9 @@ class Lazada
     arr_category = collect_category(raw_home_page)
     arr_category.each do |category|
       current_category = category[:category_name]
-      category_link = category[:category_link]
-      p current_category
-      p category_link
+      category_link = "https:" + category[:category_link].first
+      #get product paging
+      collect_product_paging(current_category, category_link)
     end
   end
 
@@ -35,7 +35,7 @@ class Lazada
           category_level1_text = cat_lv1.at_css("a span").text
           #save Category lv1 to db
           category_level1 = Category.find_or_create_by(name: category_level1_text)
-          p "start getting category lever 1 #{category_level1_text.strip}"
+          p " #{Time.zone.now} start getting category lever 1 #{category_level1_text.strip}"
 
           #start geting category level 2 belong to category lv1 with index
           ul_node = link.css(".lzd-site-menu-sub")[index]
@@ -48,24 +48,33 @@ class Lazada
               category_level2 = Category.find_or_create_by(name: sub_name, :ancestry => category_level1.id)
 
               arr_category<< {category_name: sub_name, category_link: sub_href }
-              p "start getting category lv 2: #{sub_name} link #{sub_href}"
+              p " #{Time.zone.now} start getting category lv 2: #{sub_name} link #{sub_href}"
 
             rescue
-              p "error here #{category_level1.strip}"
+              p " #{Time.zone.now} error here #{category_level1.strip}"
             end
           end
         end
 
       rescue
-        p "can't parsing category lv1"
+        p " #{Time.zone.now} can't parsing category lv1"
       end
     end
     #return category array
     arr_category
   end
 
-  def get_product_link
+  def collect_product_paging(current_category, category_link)
+    # get_total_paging = using_nokogiri(category_link).css(".ant-pagination .ant-pagination-item").first.content.to_i || 1
+    (1..10).each do |page|
+      get_product_link("#{category_link}?page=#{page}", current_category)
+    end
+  end
 
+  def get_product_link(category_link, current_category)
+    arr_product_list = []
+    document = using_nokogiri(category_link)
+    document.at('script[type="application/ld+json"]')
   end
 
 end
